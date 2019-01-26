3af013cc617866c5f5d2a0345874eb8f8841359b
m <m@ms-Mac-mini.local>
2019-01-25 20:08:33 -0800

    Updates

---
+++ precache-manifest.245e2d28170cbc3b86a8d1cbcce738e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Stacker/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "49b352346e4623061708",
+    "url": "/Stacker/static/js/runtime~main.49b35234.js"
+  },
+  {
+    "revision": "efb0b1c7da33f2885529",
+    "url": "/Stacker/static/js/main.efb0b1c7.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Stacker/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "efb0b1c7da33f2885529",
+    "url": "/Stacker/static/css/main.aa0645f5.chunk.css"
+  },
+  {
+    "revision": "1545ac4a11c9607a3084e1782cb1403e",
+    "url": "/Stacker/index.html"
+  }
+];
