60b247229e73e7d98590eb390289110f9f0a2896
m <m@ms-Mac-mini.local>
2019-01-25 19:22:44 -0800

    add index.html

--- Stacker.jsx
+++ Stacker.jsx
@@ -4,27 +4,33 @@
 var fileList;
 var outputDir;
 
-function getFileList() {
-  var selectedFolder = Folder.selectDialog( "Please select input folder");
-  if (selectedFolder !== null)  {
-    return selectedFolder.getFiles(/\.(jpg|jpeg|cr2|ARW|psd)$/i);
-  }
-}
-
-function getOutputDir() {
-  return Folder.selectDialog( "Please select an output folder");
-}
-
 function stripOutInteger(o) {
   var r = String(o)
-  r = r.replace("DSC0", "");
-  r = r.replace(".JPG", "");
   r = r.substring(r.lastIndexOf("/") + 1);
   r = r.replace(/^\D+|\D+$/g, "");
   while (r.charAt(0) == "0"){
     r = r.substring(1);
   }
   return parseInt(r);
+}
+
+function getFileList() {
+  var selectedFolder = Folder.selectDialog( "Please select input folder");
+  if (selectedFolder !== null)  {
+    let fileList = selectedFolder.getFiles(/\.(jpg|jpeg|cr2|ARW|psd)$/i);
+    if (!fileList || fileList.length < 2) {
+      return alert("Please select a folder with 2 or more files you want to stack");
+    }
+    fileList.sort(function(a, b) {
+      return stripOutInteger(a) - stripOutInteger(b);
+    });
+    printFileList(fileList);
+    return fileList
+  }
+}
+
+function getOutputDir() {
+  return Folder.selectDialog( "Please select an output folder");
 }
 
 function printFileList(fileList) {
@@ -43,20 +49,11 @@ function main() {
   if (app.documents && app.documents.length > 0) {
     return alert("Please close all open documents and run the script again!");
   }
+
   fileList = getFileList();
   if (!fileList) {
     return;
   }
-  if (fileList.length < 2) {
-    return alert("Please select a folder with 2 or more files you want to stack");
-  }
-
-  fileList.sort(function(a, b) {
-    return stripOutInteger(a) - stripOutInteger(b);
-  });
-
-  printFileList(fileList);
-
 
   outputDir = getOutputDir();
   if (!outputDir) {
