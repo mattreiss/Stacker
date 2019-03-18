04946d72bcc6340fd11e665262da56c5251d8ab5
matt <imattreiss@gmail.com>
2019-03-18 01:37:17 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+runDir=$(dirname "$0")
+
+function install_mac() {
+	app=$(ls /Applications/ | grep "Photoshop")
+	if [[ $app == *"Photoshop"* ]]; then
+		echo "Found $app"
+		echo "running cp -r $runDir/* /$app/Presets/Scripts/"
+		cp -r "$runDir" "/Applications/$app/Presets/Scripts/"
+	else
+		echo "Couldn't find Photoshop"
+	fi
+}
+if [[ "$OSTYPE" == "darwin"* ]]; then
+        install_mac
+fi
